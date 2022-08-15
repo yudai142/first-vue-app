b9e3c52babc817b6478529b5d70f1a06a2466ad3
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-08-15 22:34:16 +0900

    $ref

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+
+const form = reactive({
+  message: 'Hello World',
+});
+
+const clickButton = () => {
+  console.log(form.message);
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue 3 入門</h1>
+  <p>{{ form.message }}</p>
+  <input v-model="form.message" />
+  <div><button @click="clickButton">Click</button></div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ メモ/$ref.vue
@@ -0,0 +1,12 @@
+<script setup>
+const count = $ref(0);
+
+const addCount = () => {
+  count++;
+};
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <button type="button" @click="addCount">count is: {{ count }}</button>
+</template>
