1478a1c1035c57af84582875f2287718605bdb34
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-08-17 19:47:25 +0900

    Computed セッターbefore

---
+++ メモ/Computed セッターbefore.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive, computed } from 'vue';
+
+const user = reactive({
+  firstName: 'John',
+  lastName: 'Doe',
+});
+
+const fullName = computed(() => `${user.firstName} ${user.lastName}`);
+
+const changeName = () => {
+  fullName.value = 'Jane Doe';
+};
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <h2>fullName: {{ fullName }}</h2>
+  <button @click="changeName">Change Name</button>
+</template>
