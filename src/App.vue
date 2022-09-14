048002316c50952403b2ebf78a4be20c6e110264
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-09-14 15:03:53 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-  import { ref, computed } from 'vue';
-  import Tokyo from './components/Tokyo.vue';
-  import Kyoto from './components/Kyoto.vue';
-  
-  const city = ref('tokyo');
-  
-  const tabs = {
-    tokyo: Tokyo,
-    kyoto: Kyoto,
-  };
-  const tab = computed(() => tabs[city.value]);
+  import CompA from './components/CompA.vue';
   </script>
   
   <template>
     <h1>Vue 3 入門</h1>
-    <div>
-      <button @click="city = 'tokyo'">東京</button>
-      <button @click="city = 'kyoto'">京都</button>
-    </div>
-    <keep-alive> <component v-bind:is="city"></component></keep-alive>
+    <CompA message="propsでデータ渡し" />
   </template>
+  
