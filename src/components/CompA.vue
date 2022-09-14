b2057621407bbb7998f80179f6e433848df2abc4
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-09-14 15:02:12 +0900

    Provide/Inject

---
+++ src/components/CompA.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import CompB from './CompB.vue';
+  
+  const { message } = defineProps(['message']);
+  </script>
+  <template>
+    <h2>CompAコンポーネント</h2>
+    <CompB :message="message" />
+  </template>
---
+++ src/components/CompB.vue
@@ -0,0 +1,8 @@
+<script setup>
+  const { message } = defineProps(['message']);
+  </script>
+  
+  <template>
+    <h3>CompBコンポーネント</h3>
+    <p>{{ message }}</p>
+  </template>
